642d1e904c93fe070bb4e000fb55064804380470
RubenSehoul <sehoul.informatica@gmail.com>
2025-01-20 19:16:49 +0100

    2.5 horas de trabajo

---
+++ app/build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.sehoul.rickandmortyapi"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.sehoul.rickandmortyapi"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures{
+        viewBinding = true   //Además, hay que darle a sincronizar
+    }
+
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.activity)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    //Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+
+
+}
+
---
+++ app/src/main/java/com/sehoul/rickandmortyapi/RickandMorty.kt
@@ -0,0 +1,61 @@
+package com.sehoul.rickandmortyapi
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import com.sehoul.rickandmortyapi.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RickandMorty : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var retrofit: Retrofit
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        retrofit = getRetrofit()
+        initUI()
+    }
+
+    private fun initUI() {
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                searchByName(query.orEmpty())
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?) = false
+        })
+    }
+
+    private fun searchByName(query: String) {
+        CoroutineScope(Dispatchers.IO).launch {
+            val myResponse: Response<RickandMortyDataResponse> =
+                retrofit.create(ApiService::class.java).getRickandMorty(query)
+            // evaluando si la respuesta ha tenido éxito.
+            if (myResponse.isSuccessful) {
+                Log.i("Consulta", "Funciona :)")
+            } else {
+                Log.i("Consulta", "No funciona :(")
+                Log.i("Consulta", "No funciona :(")
+            }
+
+        }
+    }
+
+    // ASJDHKASDJ
+    private fun getRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://rickandmortyapi.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}
